ad7f2bf26ea30b2086d48586afdbb135b452d58d
Carter-Apas <57092449+Carter-Apas@users.noreply.github.com>
2026-01-31 10:58:30 +1300

    fix: Remove react from typescript config

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,4 +1,4 @@
 import common from "../lib/common.js";
 import typescript from "../lib/typescript.js";
 
-export default [...common, ...typescript, ...react];
+export default [...common, ...typescript];
